d73fd02f6d44896efa7cb94e02b4d66e81cc3244
Markus Malmgren <markus@webmalmgren.se>
2018-07-30 00:10:20 +0200

    In the middle of rsync handling

--- rsync-backup/rsync-backup.sh
+++ rsync-backup/rsync-backup.sh
@@ -9,7 +9,7 @@ set -o nounset
 
 usage() {
     declare invocation="$1"
-    echo "Usage: $invocation    : <src> <dest> [-f|--force-new] [-b|--backup-count]" >&2
+    echo "Usage: $invocation    : [-f|--force-new] [-b|--backup-count] [-r|--remote] <src> <dest>" >&2
     echo "       $invocation    : -h|--help" >&2
     echo "" >&2
     echo "Used to create backups using hard links from previous backups to optimize" >&2
@@ -19,6 +19,7 @@ usage() {
     echo "  dest:                Destination path" >&2
     echo "  -b|--backup-count:   Number of backups to store before start to remove oldest" >&2
     echo "  -f|--force-new:      Force new backup sequence" >&2
+    echo "  -r|--remote:         Destination directory on remote server, will use SSH" >&2
 }
 
 err_exit() {
@@ -85,28 +86,38 @@ delete_session() {
     rm ${session_file}
 }
 
+remote_execute() {
+    declare hostname="$1"
+    declare command="$2"
+    echo $(ssh -f "${hostname}" "${command}")
+}
+
 ############################
 # Program
 
 main() {
     declare invocation="$0"
-    declare program="$(basename $invocation)"
+    declare hostname=""
 
-    declare arguments="${1:-NO_SOURCE_PATH}"
-    declare destination_path="${2:-NO_DEST_PATH}"
-    declare backup_count="${3:-}"
+    while getopts "hr:" opt; do
+        case "${opt}" in
+        h)
+            usage ${invocation}
+            exit 0
+            ;;
+        r)
+            declare hostname=${OPTARG}
+            ;;
+        esac
+    done
+    shift $((OPTIND - 1))
 
-    case "${arguments}" in
-    --help | -h)
-        usage ${invocation}
-        exit 0
-        ;;
-    *) declare source_path="${arguments}" ;;
-    esac
-
-    if [ $# -lt 2 ]; then
+    if ! [ $# -eq 2 ]; then
         err_exit "Illegal number of arguments" "${invocation}"
     fi
+
+    declare source_path="$1"
+    declare destination_path="$2"
 
     if validate_dir "${source_path}"; then
         source_path=$(get_absolute_path "$source_path")
@@ -114,20 +125,45 @@ main() {
         err_exit "Source path not found!" "${invocation}"
     fi
 
+    if [ -z ${hostname} ]; then
+        if validate_dir "${destination_path}"; then
+            destination_path=$(get_absolute_path "${destination_path}")
+        else
+            err_exit "Destination path not found!" "${invocation}"
+        fi
+    fi
+
     echo "Source path: ${source_path}"
     echo "Destination path: ${destination_path}"
     echo ""
 
-    #echo $(create_session ${source_path} ${destination_path})
+    declare is_session_new=true
     if is_session_active "${source_path}" "${destination_path}"; then
         echo "Continuing session..."
+        is_session_new=false
     else
         echo "Starting new session..."
         create_session "${source_path}" "${destination_path}"
     fi
 
+    declare rsync_args="-avh --safe-links --delete"
+    if [ -z ${hostname} ]; then
+        declare -a backups=($(ls -1 "${destination_path}" | sort -r))
+    else
+        declare backups=($(remote_execute "${hostname}" "cd ${destination_path} && ls -1 | sort -r"))
+        rsync_args="${rsync_args} -e ssh"
+    fi
+
+    # Check if we found a previous backup, this should be used as --link-dest argument to rsync
+    if [[ ${#backups[@]} -gt 0 && is_session_new ]]; then
+        rsync_args="${rsync_args} --link-dest=../${backups[0]}"
+    fi
+
+    echo "${rsync_args}"
     # If session file exists use its data
     # If session file does not exist, extract latest backup to use as --link-desc folder
+    # rsync -avh -e ssh --safe-links --delete --link-dest=../backup-four ./source pi@git.webmalmgren.lan:~/backup-test/backup-five
+    #echo "${#backups[@]}"
 
     # Remove backup if --backup-count is exeeded
 
