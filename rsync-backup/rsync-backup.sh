8fe42f0f1113f8c32c37b118b74bd744e8992fbc
Markus Malmgren <markus@webmalmgren.se>
2018-07-30 15:26:52 +0200

    Works with local backup.

--- rsync-backup/rsync-backup.sh
+++ rsync-backup/rsync-backup.sh
@@ -50,13 +50,13 @@ get_system_temp_dir() {
 
 md5_from_paths() {
     declare source_path="$1"
-    declare destination_path="$2"
+    declare backup_root_path="$2"
     if [[ $(uname) == "Darwin"* ]]; then
         declare md5="md5"
     else
         declare md5="md5sum"
     fi
-    echo $(echo "src: ${source_path} dst: ${destination_path}" | ${md5} | sed -E 's/(^[a-f|0-9])*.$/\1/')
+    echo $(echo "src: ${source_path} dst: ${backup_root_path}" | ${md5} | sed -E 's/(^[a-f|0-9])*.$/\1/')
 }
 
 get_timestamp() {
@@ -65,28 +65,28 @@ get_timestamp() {
 
 get_session_filename() {
     declare source_path="$1"
-    declare destination_path="$2"
-    echo "$(get_system_temp_dir)/rsync-$(md5_from_paths ${source_path} ${destination_path})"
+    declare backup_root_path="$2"
+    echo "$(get_system_temp_dir)/rsync-$(md5_from_paths ${source_path} ${backup_root_path})"
 }
 
 create_session() {
     declare source_path="$1"
-    declare destination_path="$2"
-    declare session_file="$(get_session_filename ${source_path} ${destination_path})"
+    declare backup_root_path="$2"
+    declare session_file="$(get_session_filename ${source_path} ${backup_root_path})"
     echo "session_started=$(get_timestamp)" >"${session_file}"
 }
 
 is_session_active() {
     declare source_path="$1"
-    declare destination_path="$2"
-    declare session_file="$(get_session_filename ${source_path} ${destination_path})"
+    declare backup_root_path="$2"
+    declare session_file="$(get_session_filename ${source_path} ${backup_root_path})"
     [[ -f "${session_file}" ]]
 }
 
 delete_session() {
     declare source_path="$1"
-    declare destination_path="$2"
-    declare session_file="$(get_session_filename ${source_path} ${destination_path})"
+    declare backup_root_path="$2"
+    declare session_file="$(get_session_filename ${source_path} ${backup_root_path})"
     rm ${session_file}
 }
 
@@ -121,7 +121,7 @@ main() {
     fi
 
     declare source_path="$1"
-    declare destination_path="$2"
+    declare backup_root_path="$2"
 
     if validate_dir "${source_path}"; then
         source_path=$(get_absolute_path "$source_path")
@@ -130,58 +130,53 @@ main() {
     fi
 
     if [ -z ${hostname} ]; then
-        if validate_dir "${destination_path}"; then
-            destination_path=$(get_absolute_path "${destination_path}")
+        if validate_dir "${backup_root_path}"; then
+            backup_root_path=$(get_absolute_path "${backup_root_path}")
         else
             err_exit "Destination path not found!" "${invocation}"
         fi
     fi
 
     echo "Source path: ${source_path}"
-    echo "Destination path: ${destination_path}"
-    echo ""
+    echo "Backup(s) root path: ${backup_root_path}"
 
     declare new_session=true
-    if is_session_active "${source_path}" "${destination_path}"; then
-        echo "Continuing session..."
+    if is_session_active "${source_path}" "${backup_root_path}"; then
+        echo "Session: Continue previous backup"
         new_session=false
     else
-        echo "Starting new session..."
-        create_session "${source_path}" "${destination_path}"
+        echo "Session: New backup"
+        create_session "${source_path}" "${backup_root_path}"
     fi
 
     declare rsync_args="-avh --safe-links --delete"
     if [ -z ${hostname} ]; then
-        declare -a backups=($(ls -1 "${destination_path}" | sort -r))
+        declare -a backups=($(ls -1 "${backup_root_path}" | sort -r))
     else
-        declare backups=($(remote_execute "${hostname}" "cd ${destination_path} && ls -1 | sort -r"))
+        declare backups=($(remote_execute "${hostname}" "cd ${backup_root_path} && ls -1 | sort -r"))
         rsync_args="${rsync_args} -e ssh"
     fi
 
     # Check if we found a previous backup, this should be used as --link-dest argument to rsync
-    #if [[ ${#backups[@]} -gt 1 && ! new_session ]]; then
-    #    rsync_args="${rsync_args} --link-dest=../${backups[0]}"
-    #fi
-    echo "${new_session}"
-    if new_session; then
-        echo "Session is new"
+    if [ "$new_session" = true ]; then
         if [[ ${#backups[@]} > 0 ]]; then
             rsync_args="${rsync_args} --link-dest=../${backups[0]}"
         fi
+        destination_path=${backup_root_path}/$(get_timestamp)
+        mkdir ${destination_path}
     else
         if [[ ${#backups[@]} > 1 ]]; then
-            rsync_args="${rsync_args} --link-dest=../${backups[0]}"
+            rsync_args="${rsync_args} --link-dest=../${backups[1]}"
         fi
+        destination_path=${backup_root_path}/${backups[0]}
     fi
+    #echo "--- sleep 5 ---"
+    #sleep 5
 
-    # If session is new, check num backups > 0 => use link-dest
-    # else (Session is not new), check if num backups > 1 => use link-dest
+    echo "Executing: rsync ${rsync_args} ${source_path} ${destination_path}"
+    rsync ${rsync_args} "${source_path}" "${destination_path}"
 
-    echo "${rsync_args}"
-    # If session file exists use its data
-    # If session file does not exist, extract latest backup to use as --link-desc folder
-    # rsync -avh -e ssh --safe-links --delete --link-dest=../backup-four ./source pi@git.webmalmgren.lan:~/backup-test/backup-five
-    #echo "${#backups[@]}"
+    delete_session "${source_path}" "${backup_root_path}"
 
     # Remove backup if --backup-count is exeeded
     # Break out host tools
